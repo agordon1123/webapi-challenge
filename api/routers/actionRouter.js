7f14dcd699e1f7691cd528ac5c890bc5df016ae8
Alex <agordon1123@gmail.com>
2019-09-06 14:04:50 -0400

    CRUD endpoints complete

--- api/routers/actionRouter.js
+++ api/routers/actionRouter.js
@@ -4,6 +4,19 @@ const projectsdb = require('../../data/helpers/projectModel');
 
 const actionRouter = express.Router();
 
+
+// CREATE
+actionRouter.post('/', validateActionBody, (req, res) => {
+    db.insert(req.body)
+    .then(suc => {
+        res.status(201).json(suc);
+    })
+    .catch(() => {
+        res.status(500).json({ error: 'Internal server error' });
+    })
+});
+
+// READ
 actionRouter.get('/', (req, res) => {
     db.get()
         .then(actions => {
@@ -25,11 +38,40 @@ actionRouter.get('/:id', (req, res) => {
         })
 });
 
+// UPDATE
+actionRouter.put('/:id', validateActionBody, (req, res) => {
+    const { id } = req.params;
+    const content = req.body;
+
+    db.update(id, content)
+        .then(suc => {
+            res.status(200).json(suc)
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'Internal server error' })
+        })
+})
+
+// DELETE
 actionRouter.delete('/:id', (req, res) => {
     const { id } = req.params;
     db.remove(id)
-        .then()
-        .catch()
+        .then(suc => {
+            res.status(204).send(`Successfully deleted the action with the ID: ${id}`)
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'Internal server error' })
+        })
 })
+
+// Middleware
+function validateActionBody(req, res, next) {
+    if (req.body.notes === undefined || req.body.description === undefined || req.body.project_id === undefined) {
+        res.status(400).json({ error: 'Please include a "notes" string, "description" string, and "completed" boolean with your POST request' })
+    } else {
+        next();
+    }
+}
+
 
 module.exports = actionRouter;
--- api/routers/projectRouter.js
+++ api/routers/projectRouter.js
@@ -1,32 +1,11 @@
 const express = require('express');
 const db = require('../../data/helpers/projectModel');
+const actionRouter = require('./actionRouter');
 
 const projectRouter = express.Router();
+projectRouter.use('/:id/actions', actionRouter);
 
-// GET
-projectRouter.get('/', (req, res) => {
-    db.get()
-        .then(projects => {
-            res.status(200).json(projects);
-        })
-        .catch(() => {
-            res.status(500).json({ error: 'Internal server error' });
-        })
-});
-
-// DELETE
-projectRouter.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    db.remove(id)
-        .then(suc => {
-            res.status(200).json(suc);
-        })
-        .catch(() => {
-            res.status(500).json({ error: 'Internal server error' });
-        })
-});
-
-// POST
+// CREATE
 projectRouter.post('/', validateProjectBody, (req, res) => {    
     db.insert(req.body)
         .then(suc => {
@@ -37,6 +16,29 @@ projectRouter.post('/', validateProjectBody, (req, res) => {
         })
 });
 
+// READ
+projectRouter.get('/', (req, res) => {
+    db.get()
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'Internal server error' });
+        })
+});
+
+projectRouter.get('/:id', (req, res) => {
+    const { id } = req.params;
+    db.get(id)
+        .then(project => {
+            res.status(200).json(project);
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'Internal server error' });
+        })
+})
+
+
 // UPDATE
 projectRouter.put('/:id', (req, res) => {
     const { id } = req.params;
@@ -45,21 +47,35 @@ projectRouter.put('/:id', (req, res) => {
         res.status(400).json({ error:  'Please include a "name" string and "description" string with your PUT request' })
     }
     db.update(id, content)
-        .then(suc => {
-            res.status(201).json(suc)
+    .then(suc => {
+        res.status(201).json(suc)
         })
         .catch(() => {
             res.status(500).json({ error: 'Internal server error' })
         })
-})
-
-// Middleware
-function validateProjectBody(req, res, next) {
-    if (req.body.name === undefined || req.body.description === undefined) {
-        res.status(400).json({ error: 'Please include a "name" string, "description" string, and "completed" boolean with your POST request' })
-    } else {
-        next();
+    });
+    
+    
+    // DELETE
+    projectRouter.delete('/:id', (req, res) => {
+        const { id } = req.params;
+        db.remove(id)
+        .then(suc => {
+            res.status(200).json(suc);
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'Internal server error' });
+        })
+    });
+        
+    // Middleware
+    function validateProjectBody(req, res, next) {
+        if (req.body.name === undefined || req.body.description === undefined) {
+            res.status(400).json({ error: 'Please include a "name" string, "description" string, and "completed" boolean with your POST request' })
+        } else {
+            next();
+        }
     }
-}
-
-module.exports = projectRouter;
+    
+    module.exports = projectRouter;
+    
