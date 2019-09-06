1cd750dec0d8b52e52dde3e9a36272405c209de3
Alex <agordon1123@gmail.com>
2019-09-06 12:56:36 -0400

    restructured endpoints

--- api/routers/projectRouter.js
+++ api/routers/projectRouter.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const db = require('../../data/helpers/projectModel');
+const actionRouter = require('./actionRouter');
 
 const projectRouter = express.Router();
+
+projectRouter.use('/:id/actions', actionRouter)
 
 projectRouter.get('/', (req, res) => {
     db.get()
@@ -12,5 +15,37 @@ projectRouter.get('/', (req, res) => {
             res.status(500).json({ error: 'Internal server error' });
         })
 });
+
+projectRouter.get('/:id', (req, res) => {
+    const { id } = req.params;
+    db.getProjectActions(id)
+        .then(actions => {
+            res.status(200).json(actions)
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'Internal server error' })
+        })
+})
+
+// projectRouter.delete('/', (req, res) => {
+//     const { id } = req.params;
+//     db.remove(id)
+//         .then()
+//         .catch()
+// })
+
+// projectRouter.post('/', (req, res) => {    
+    // if (!req.body.name || !req.body.description) {
+    //     res.status(400).json({ error: 'Please include a "name" string, "description" string, and "completed" boolean with your POST request' })
+    // } else {
+        // db.insert(req.body)
+        //     .then(suc => {
+        //         res.status(201).json(suc);
+        //     })
+        //     .catch(() => {
+        //         res.status(500).json({ error: 'Internal server error' });
+        //     })
+    // }
+// })
 
 module.exports = projectRouter;
--- api/server.js
+++ api/server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const projectRouter = require('./routers/projectRouter');
-const actionRouter = require('./routers/actionRouter');
 
 const server = express();
 
 server.use('/projects', projectRouter);
-server.use('/actions', actionRouter);
 
 module.exports = server;
